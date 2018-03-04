2ac980b4edf291aac664ccac4ea8e115d3e9c06e
rmurph29 <ryan.m.murphy@slu.edu>
2018-03-04 10:07:05 -0600

    Add location help button

--- client/src/components/AddRide.js
+++ client/src/components/AddRide.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
 import RideService from './RideService';
 import {Link} from 'react-router-dom';
+import {geolocated, geoPropTypes} from 'react-geolocated';
+
 let locations = require('../locations');
+
 
 class AddRide extends Component {
 
@@ -10,11 +13,19 @@ class AddRide extends Component {
 
         let now = Date.now();
 
-        this.state = {pickup:"BSC", dropoff:"BSC", received:now, riders:"1"};
+        this.state = {pickup:"BSC", dropoff:"BSC", received:now, riders:"1", advice:"Use Location to Advise Pickup",
+        lat: 0, long: 0};
         this.addRideService = new RideService();
+        this.geoOptions = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        };
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleGeolocate = this.handleGeolocate.bind(this);
+        this.findClosest = this.findClosest.bind(this);
         this.validateForm = this.validateForm.bind(this);
         this.populateLocations = this.populateLocations.bind(this);
         this.addRideService.sendData = this.addRideService.sendData.bind(this);
@@ -23,7 +34,7 @@ class AddRide extends Component {
     validateForm() {
         let bannerPattern = new RegExp("00[0-9]{7}");
         let phonePattern = new RegExp("[0-9]{10}");
-        let emailPattern = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+        let emailPattern = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
         let riderPattern = new RegExp("[1-6]");
 
         if(!bannerPattern.test(this.state.banner)){
@@ -49,11 +60,11 @@ class AddRide extends Component {
 
     populateLocations() {
         return locations.map(function(location, i){
-            if(location === "Frost Campus" || location === "Medical Campus" || location === "Off Campus" || location === "Intersections"){
-                return <option value={location} key={i} disabled>{location}</option>;
+            if(location[0] === "Frost Campus" || location[0] === "Medical Campus" || location[0] === "Off Campus" || location[0] === "Intersections"){
+                return <option value={location[0]} key={i} disabled>{location[0]}</option>;
             }
             else{
-                return <option value={location} key={i}>{location}</option>;
+                return <option value={location[0]} key={i}>{location[0]}</option>;
             }
         })
     }
@@ -70,15 +81,87 @@ class AddRide extends Component {
         event.preventDefault();
         if(this.validateForm()) {
             this.addRideService.sendData(this.state);
-            console.log(this.state.id)
+            console.log(this.state.id);
             this.props.history.push('/complete/');
         }
+    }
+
+    findClosest(pos){
+//        let lat = pos.coords.latitude;
+//        let long = pos.coords.longitude;
+        let lat = '38.634830';
+        let long = '-90.224989';
+
+
+        let min = 99999;
+        let closest;
+        let index;
+
+        for (index = 0; index < locations.length; ++index) {
+
+            let lat2 = locations[index][1];
+            if( lat2 !== 'NA') {
+
+                let dif = getDist(lat, long, locations[index][1], locations[index][2]);
+                if (dif < min) {
+                    closest = index;
+                    min = dif;
+                }
+            }
+        }
+
+        function getDist(lat1, long1, lat2, long2){
+            let R = 6371; // Radius of the earth in km
+            let dLat = degToRad(lat2-lat1);  // deg2rad below
+            let dLon = degToRad(long2-long1);
+            let a =
+                Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) *
+                Math.sin(dLon/2) * Math.sin(dLon/2)
+            ;
+            let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+         //   let d = R * c; // Distance in km
+            return R * c;
+        }
+
+
+        function degToRad(deg){
+            return deg * (Math.PI/180);
+        }
+
+        const newText = locations[closest][0];
+        this.setState({advice: newText});
+    }
+
+    handleGeolocate() {
+
+        if ("geolocation" in navigator) {
+            /* geolocation is available */
+
+            const geo = navigator.geolocation;
+
+            function fail(err) {
+                console.warn(`ERROR(${err.code}): ${err.message}`);
+            }
+
+            geo.getCurrentPosition( this.findClosest, fail , this.geoOptions );
+
+        } else {
+            /* geolocation IS NOT available */
+            const newText = 'fail';
+            this.setState({advice: newText});
+        }
+
     }
 
     render(){
         return(
                 <div className="main-content" padding="5">
                     <h1>Request a Ride!</h1><hr />
+                    <h2><input type = "button" className = "button" value = {this.state.advice} onClick = {this.handleGeolocate} /></h2>
+                    <br/>
+                    <br/>
+
                     <form onSubmit={this.handleSubmit}>
                         <h4>Name:</h4>
                                 <input name="name" type="text" value={this.state.name} placeholder="Full Name" onChange={this.handleInputChange} className="form-control" required />
@@ -115,4 +198,8 @@ class AddRide extends Component {
     }
 }
 
-export default AddRide;
+
+AddRide.propTypes = Object.assign({}, AddRide.propTypes, geoPropTypes);
+
+//export default AddRide;
+export default geolocated()(AddRide);
